5d425a967a25b806826d5993b8b88ddf4a1682e4
khuong <dinhquangkhuong5566@gmail.com>
2025-10-15 12:14:29 +0700

    remove failed

--- audio-worklet/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# cargo build --target wasm32-unknown-unknown --release
-# wasm-opt ../target/wasm32-unknown-unknown/release/df_audio_worklet.wasm -o dsp.wasm -Os
-
-set -ex
-
-python3 build.py
